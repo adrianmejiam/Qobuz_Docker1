8e68d21c77440f4e60d410d24c295aab2a29ec3f
Adrián Mejía <51228667+adrianmejiam@users.noreply.github.com>
2025-07-17 04:57:55 -0600

    main.py

--- main.py
+++ main.py
@@ -105,6 +105,13 @@ def init_qobuz(email, password):
         bundle = Bundle()
 app_id = bundle.get_app_id()
 secrets = "\n".join(bundle.get_secrets().values())
+
+print(f"App ID: {app_id}")
+
+print("#" * 20)
+
+print(f"Secrets (the first usually works):{secrets}")
+
     except:
         logger.error('Wrong Credentials')
 
