595a66ae1ddc57d0bd85539b8a20787a08f3b6f4
Adrián Mejía <51228667+adrianmejiam@users.noreply.github.com>
2025-07-17 04:44:09 -0600

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ from cherrypy.lib import static
 from qobuz_dl.core import QobuzDL
 from qobuz_dl.bundle import Bundle
 
+bundle = Bundle()
+app_id = bundle.get_app_id()
+secrets = "\n".join(bundle.get_secrets().values())
+
+print(f"App ID: {app_id}")
+
+print("#" * 20)
+
+print(f"Secrets (the first usually works):{secrets}")
+
 
 # Register your APP_ID
 qobuz.register_app("YOUR_APP_ID")
