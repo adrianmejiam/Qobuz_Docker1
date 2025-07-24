2d6a58fcb1b35c24414ebfd934053c557a7fcdc1
Adrián Mejía <51228667+adrianmejiam@users.noreply.github.com>
2025-07-24 09:51:52 -0600

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import cherrypy
 from cherrypy.lib import static
 from qobuz_dl.core import QobuzDL
 import base64
-import logging
 from collections import OrderedDict
 
 from requests import Session
@@ -37,57 +36,6 @@ _BUNDLE_URL_REGEX = re.compile(
     r'<script src="(/resources/\d+\.\d+\.\d+-[a-z]\d{3}/bundle\.js)"></script>'
 )
 
-
-class Bundle:
-    def __init__(self):
-        self._session = Session()
-
-        logger.debug("Getting logging page")
-        response = self._session.get(f"{_BASE_URL}/login")
-        response.raise_for_status()
-
-        bundle_url_match = _BUNDLE_URL_REGEX.search(response.text)
-        if not bundle_url_match:
-            raise NotImplementedError("Bundle URL found")
-
-        bundle_url = bundle_url_match.group(1)
-
-        logger.debug("Getting bundle")
-        response = self._session.get(_BASE_URL + bundle_url)
-        response.raise_for_status()
-
-        self._bundle = response.text
-
-    def get_app_id(self):
-        match = _APP_ID_REGEX.search(self._bundle)
-        if not match:
-            raise NotImplementedError("Failed to match APP ID")
-
-        return match.group("app_id")
-
-    def get_secrets(self):
-        logger.debug("Getting secrets")
-        seed_matches = _SEED_TIMEZONE_REGEX.finditer(self._bundle)
-        secrets = OrderedDict()
-
-        for match in seed_matches:
-            seed, timezone = match.group("seed", "timezone")
-            secrets[timezone] = [seed]
-
-        keypairs = list(secrets.items())
-        secrets.move_to_end(keypairs[1][0], last=False)
-        info_extras_regex = _INFO_EXTRAS_REGEX.format(
-            timezones="|".join([timezone.capitalize() for timezone in secrets])
-        )
-        info_extras_matches = re.finditer(info_extras_regex, self._bundle)
-        for match in info_extras_matches:
-            timezone, info, extras = match.group("timezone", "info", "extras")
-            secrets[timezone.lower()] += [info, extras]
-        for secret_pair in secrets:
-            secrets[secret_pair] = base64.standard_b64decode(
-                "".join(secrets[secret_pair])[:-44]
-            ).decode("utf-8")
-        return secrets
 
 logging.basicConfig(level=logging.INFO)
 
